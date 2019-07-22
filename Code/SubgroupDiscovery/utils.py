2eb6a43ed481d0de331a0b9985f89015f3cebbb4
Xiaoqi Ma <xiaoqima2013@gmail.com>
2019-07-22 14:41:25 +0200

    modify notebook files

--- Code/SubgroupDiscovery/utils.py
+++
@@ -1,9 +0,0 @@
-from sklearn import preprocessing
-from pandas.api.types import is_numeric_dtype
-
-
-def normalize_data(data):
-    df = data.copy()
-    num_features = [c for c in df.columns if is_numeric_dtype(df[c])]
-    df[num_features] = preprocessing.scale(df[num_features])
-    return df
